ab8eec74e382aa2a0d673fe79645f849a0401ce0
Sergio A Valdes Rabelo <sergiovades2409@gmail.com>
2022-01-15 16:11:49 -0700

    Include another example Template method pattern

---
+++ behavioral/template_method/py/trading_bot/trading_bot_after.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -----------------------------------------------
+# Copyright (c) ░s░e░r░g░i░o░v░a░l░d░e░s░2░4░0░9░
+# Mail: sergiovaldes2409@gmail.com
+#
+# All rights reserved.
+#
+#
+from typing import List
+from abc import ABC, abstractmethod
+
+
+class TradingBot(ABC):
+
+    @abstractmethod
+    def should_buy(self, prices: List[float]) -> bool:
+        pass
+
+    @abstractmethod
+    def should_sell(self, prices: List[float]) -> bool:
+        pass
+
+    def connect(self):
+        print(f"Connecting to Crypto exchange...")
+
+    def get_market_data(self, coin: str) -> List[float]:
+        return [10, 12, 18, 14]
+
+    def check_prices(self, coin: str):
+        self.connect()
+        prices = self.get_market_data(coin)
+        should_buy = self.should_buy(prices)
+        should_sell = self.should_sell(prices)
+        if should_buy:
+            print(f"You should buy {coin}!")
+        elif should_sell:
+            print(f"You should sell {coin}!")
+        else:
+            print(f"No action needed for {coin}.")
+
+
+class AverageTrader(TradingBot):
+
+    def list_average(self, l: List[float]) -> float:
+        return sum(l) / len(l)
+
+    def should_buy(self, prices: List[float]) -> bool:
+        return prices[-1] < self.list_average(prices)
+
+    def should_sell(self, prices: List[float]) -> bool:
+        return prices[-1] > self.list_average(prices)
+
+
+class MinMaxTrader(TradingBot):
+
+    def should_buy(self, prices: List[float]) -> bool:
+        return prices[-1] == min(prices)
+
+    def should_sell(self, prices: List[float]) -> bool:
+        return prices[-1] == max(prices)
+
+
+if __name__ == '__main__':
+    average_trader = AverageTrader()
+    average_trader.check_prices("BTC/USD")
+    min_max_trader = MinMaxTrader()
+    min_max_trader.check_prices("BTC/USD")
